dc837620cb065657f6f0a22eaba4076606d55358
jhenrique09 <jhenrique09.mcz@hotmail.com>
2020-01-30 03:44:46 +0530

    sdm660-common: Add safetynet hack
    Change-Id: I5ead42b304820e6ae672c8644e2541e96ad09f1e

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -114,6 +114,7 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
+<<<<<<< HEAD
 LOCAL_MODULE       := init.performance_level.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
@@ -128,4 +129,11 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE       := init.safailnet.rc
+LOCAL_MODULE_TAGS  := optional
+LOCAL_MODULE_CLASS := ETC
+LOCAL_SRC_FILES    := etc/init.safailnet.rc
+LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
