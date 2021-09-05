814ab6d6592d09f52c61e32a833605522ea6705d
harshaxvarthan <harshaxvarthan@gmail.com>
2021-09-05 17:56:40 +0530

    welcome to project-xtended

--- xtended_vayu.mk
+++ xtended_vayu.mk
@@ -1,5 +1,5 @@
 #
-# Copyright (C) 2018-2021 ArrowOS
+# Copyright (C) 2018-2021 Project Xtended
 #
 # SPDX-License-Identifier: Apache-2.0
 #
@@ -11,10 +11,17 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit device configurations
 $(call inherit-product, device/xiaomi/vayu/device.mk)
 
-# Inherit common ArrowOS configurations
-$(call inherit-product, vendor/arrow/config/common.mk)
+# Inherit some common Xtended stuff.
+$(call inherit-product, vendor/xtended/config/common_full_phone.mk)
 
-PRODUCT_NAME := arrow_vayu
+# Gapps
+WITH_GAPPS := true
+TARGET_FACE_UNLOCK_SUPPORTED := true
+XTENDED_BUILD_TYPE := UNOFFICIAL
+XTENDED_BUILD_MAINTAINER := harshaxvarthan
+TARGET_BOOT_ANIMATION_RES := 1080
+
+PRODUCT_NAME := xtended_vayu
 PRODUCT_DEVICE := vayu
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := Poco X3 Pro
@@ -26,6 +33,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRIVATE_BUILD_DESC="vayu_global-user 11 RKQ1.200826.002 V12.5.2.0.RJUMIXM release-keys"
 
 BUILD_FINGERPRINT := POCO/vayu_global/vayu:11/RKQ1.200826.002/V12.5.2.0.RJUMIXM:user/release-keys
-
-DEVICE_MAINTAINER := kubersharma001
 
