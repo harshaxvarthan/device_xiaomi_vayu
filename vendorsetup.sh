7f53a4201479858972a3c5a6fc81da86edf4761d
harshaxvarthan <82403923+harshaxvarthan@users.noreply.github.com>
2021-10-06 18:10:37 +0530

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -3,5 +3,5 @@ rm -rf hardware/xiaomi
 
 # Clone HAL's
 git clone https://github.com/ArrowOS-Devices/android_vendor_xiaomi_vayu -b arrow-11.0 vendor/xiaomi/vayu
-git clone https://github.com/ArrowOS-Devices/android_kernel_xiaomi_vayu -b arrow-11.0 kernel/xiaomi/vayu
+git clone https://github.com/nullxception/kernel_xiaomi_vayu -b eleven kernel/xiaomi/vayu
 git clone https://github.com/ArrowOS-Devices/android_hardware_xiaomi -b arrow-11.0 hardware/xiaomi
