b6d3b2af289c46d4ad689ee7f46dba0b8a95526d
harshaxvarthan <harshaxvarthan@gmail.com>
2021-10-09 17:09:17 +0000

    Revert "prebuilts"
    This reverts commit 3e71a27378ff514c2a35e9c9aad6ee4437d5ce25.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -79,6 +79,7 @@ BOARD_HAVE_QCOM_FM := true
 # Input
 TARGET_INPUTDISPATCHER_SKIP_EVENT_KEY := 304
 
+<<<<<<< HEAD
 # Kernel-Prebuilt
 BOARD_PREBUILT_DTBOIMAGE := $(LOCAL_PATH)/kernel-pb/dtbo.img
 TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)/kernel-pb/Image
@@ -92,8 +93,11 @@ DEVICE_PREBUILT_PATH := device/xiaomi/vayu/prebuilt
 # Kernel
 BOARD_KERNEL_BASE := 0x00000000
 
+=======
+# Kernel
+ARD_KERNEL_BASE := 0x00000000
+>>>>>>> parent of 3e71a27 (prebuilts)
 BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=1
-
 BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=2048 msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
@@ -101,7 +105,14 @@ BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
 BOARD_KERNEL_PAGESIZE := 4096
 BOARD_BOOTIMG_HEADER_VERSION := 2
 BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+<<<<<<< HEAD
 
+=======
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/kernel-pb/Image
+TARGET_PREBUILT_DTB := $(DEVICE_PATH)/kernel-pb/dtb.img
+LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+>>>>>>> parent of 3e71a27 (prebuilts)
 TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_IMAGE_NAME := Image
 ifeq ($(TARGET_PREBUILT_KERNEL),)
