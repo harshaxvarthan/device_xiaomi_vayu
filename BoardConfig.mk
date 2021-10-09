e06078f26ce211540022f7e9b1f6355d3cd17af6
harshaxvarthan <82403923+harshaxvarthan@users.noreply.github.com>
2021-10-09 22:04:05 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -89,7 +89,30 @@ DEVICE_PREBUILT_PATH := device/xiaomi/vayu/prebuilt
 
 
 
+# Kernel
+BOARD_KERNEL_BASE := 0x00000000
 
+BOARD_KERNEL_CMDLINE := androidboot.hardware=qcom androidboot.memcg=1
+
+BOARD_KERNEL_CMDLINE += lpm_levels.sleep_disabled=1 service_locator.enable=1
+BOARD_KERNEL_CMDLINE += swiotlb=2048 msm_rtb.filter=0x237
+BOARD_KERNEL_CMDLINE += loop.max_part=7 androidboot.usbcontroller=a600000.dwc3
+BOARD_KERNEL_CMDLINE += androidboot.init_fatal_reboot_target=recovery
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_BOOTIMG_HEADER_VERSION := 2
+BOARD_MKBOOTIMG_ARGS := --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+
+TARGET_KERNEL_ARCH := arm64
+BOARD_KERNEL_IMAGE_NAME := Image
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+  TARGET_KERNEL_CONFIG := vayu_user_defconfig
+  TARGET_KERNEL_CLANG_COMPILE := true
+  TARGET_KERNEL_SOURCE := kernel/xiaomi/vayu
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel \
+    device/xiaomi/vayu/prebuilt/dtb.img:dtb.img
 
 
 # NFC
