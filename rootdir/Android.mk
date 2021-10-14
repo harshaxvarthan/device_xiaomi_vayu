b3fbbe9f83225828744dc7eaa2aca2483223959b
harshaxvarthan <harshaxvarthan@gmail.com>
2021-10-15 00:38:29 +0530

    cts hack: fix typos

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -114,7 +114,6 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR_ETC)/init/hw
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-<<<<<<< HEAD
 LOCAL_MODULE       := init.performance_level.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
@@ -129,6 +128,7 @@ LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
 LOCAL_SRC_FILES    := etc/ueventd.qcom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_VENDOR)
+include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE       := init.safailnet.rc
