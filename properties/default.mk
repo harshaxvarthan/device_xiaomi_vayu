792881fddc235151810d0b5605d2d631b2118ab6
harshaxvarthan <harshaxvarthan@gmail.com>
2021-10-15 16:14:03 +0000

    Revert "Fix messed up sf"
    This reverts commit 342ee35d7cfda3b5add7919cc762b6f6768cb235.

--- properties/default.mk
+++ properties/default.mk
@@ -15,7 +15,6 @@
 #
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    ro.surface_flinger.force_hwc_copy_for_virtual_displays=true \
     ro.surface_flinger.max_virtual_display_dimension=4096 \
     ro.surface_flinger.has_HDR_display=true \
     ro.surface_flinger.has_wide_color_display=true \
@@ -25,7 +24,9 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     ro.surface_flinger.set_idle_timer_ms=500 \
     ro.surface_flinger.set_touch_timer_ms=750 \
     ro.surface_flinger.use_color_management=true \
-    ro.surface_flinger.use_content_detection_for_refresh_rate=true
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
+    vendor.display.idle_time=0 \
+    vendor.display.idle_time_inactive=0
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.early_app_phase_offset_ns=500000 \
