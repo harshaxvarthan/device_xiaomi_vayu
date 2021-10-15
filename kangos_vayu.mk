5b51eac5ebaa9a25c1375c07eb406582a69a7ddf
harshaxvarthan <harshaxvarthan@gmail.com>
2021-10-15 06:04:48 +0000

    adapt

--- kangos_vayu.mk
+++ kangos_vayu.mk
@@ -13,6 +13,8 @@ $(call inherit-product, device/xiaomi/vayu/device.mk)
 
 # Inherit some common kangos stuff.
 $(call inherit-product, vendor/kangos/config/common.mk)
+$(call inherit-product, vendor/gms/gms_full.mk)
+ 
 KANGOS_BUILDTYPE := UNOFFICIAL
 PRODUCT_PRODUCT_PROPERTIES += \
   ro.kangos.maintainer=darkinvader \
